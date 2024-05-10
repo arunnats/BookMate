e4ce4a269b8e7afc15536e91cb0f6fcd1380738d
arunnats <arun_b220019cs@nitc.ac.in>
2024-05-10 23:17:39 +0530

    Decided to use an elasticsearch engine instead. seems promising, working out the indexing. Connections are set up seem to be integrated

--- Database/OpenLibraryDatabaseScraping/OpenLibraryDumpProcessChunked.py
+++ Database/OpenLibraryDatabaseScraping/OpenLibraryDumpProcessChunked.py
@@ -47,9 +47,8 @@ def run():
                     json_data = json.loads(row[4])
 
                     if 'title' in json_data and 'subjects' in json_data:
-                    #Check if any of the required subjects are present
                         interested_subjects = {'fiction', 'romance', 'young adult', 'fantasy', 'American drama', 'FICTION / Thrillers / General', 'Fiction, thrillers, suspense', 'FICTION / Thrillers / Suspense', 'Suspense', 'Thrillers', 'Action and adventure fiction', 'Romans, nouvelles', 'Fantasy fiction', 'Fiction, fantasy, general', 'Young adult fiction', 'Teenage girls', 'Juvenile fiction', 'History', 'Romance', 'literary fiction', 'FICTION / Romance / Contemporary', 'Fiction, erotica, general', 'Love stories', 'Erotic Fiction', 'Imaginary places', 'action & adventure', 'Epic', 'Epic literature', 'Juvenile Fantasy fiction', 'Fantasy', 'Adventure', 'fiction mystery & detective traditional', 'crime novel', 'crime', 'novel', 'Hindu mythology', 'Fiction, romance, general', 'romance', 'dark fantasy', 'Young adult fiction', 'post-apocalyptic', 'gothic', 'horror', 'Horror fiction', 'Horror tales', 'Science fiction', 'Homosexuality', 'Coming of age', 'Good and evil', 'Time travel', 'Reading Level-Grade 9', 'Reading Level-Grade 8', 'Reading Level-Grade 11', 'Reading Level-Grade 10', 'Reading Level-Grade 12', 'alternative history', 'Romantic Comedy', 'comics', 'Comic books, strips', 'Friendship', 'Mystery'}
-                        if any(subject.lower() in interested_subjects for subject in json_data['subjects']):
+                        if any(subject.lower() in map(str.lower, interested_subjects) or any(interested_subj.lower() in subject.lower() for interested_subj in interested_subjects) for subject in json_data['subjects']):
                             if 'languages' in json_data and any(language.get('key', '').endswith('/eng') for language in json_data['languages']):
                                 title = json_data['title']
                                 isbn = json_data['isbn_13'][0] if 'isbn_13' in json_data else ''
