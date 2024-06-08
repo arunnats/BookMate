53cf5275052abff5597145250b340cde19b464b4
arunnats <arun_b220019cs@nitc.ac.in>
2024-06-08 22:12:09 +0530

    created vite app

---
+++ App/frontend/BookMate/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div>
+				<h3 className="text-blue-900 text-5xl justify-center align-middle">
+					Hai :3
+				</h3>
+			</div>
+		</>
+	);
+}
+
+export default App;
