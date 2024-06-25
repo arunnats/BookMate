4b38f9b6b4ff3ed3160cbdc26803ff57d20e25ad
arunnats <arun_b220019cs@nitc.ac.in>
2024-06-26 00:06:40 +0530

    Update QuestionData.jsx

--- App/frontend/BookMate/src/components/QuestionData.jsx
+++ App/frontend/BookMate/src/components/QuestionData.jsx
@@ -2,11 +2,56 @@ const QuestionsData = [
 	{
 		clusterId: 1,
 		questions: [
-			{ id: 1, question: "Question 1?", options: ["A", "B", "C", "D"] },
-			{ id: 2, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 3, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 4, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 5, question: "Question 2?", options: ["A", "B", "C", "D"] },
+			{
+				id: 1,
+				question: "Which character would you most like to meet IRL?",
+				options: [
+					"A) Percy Jackson from the 'Camp Half-Blood Chronicles'",
+					"B) Robert Langdon from the Dan Browns books",
+					"C) Atlas Corrigan from 'It Ends With Us'",
+					"D) Raskolnikov from 'Crime and Punishment'",
+				],
+			},
+			{
+				id: 2,
+				question: "Which book-to-movie adaptation do you enjoy the most?",
+				options: [
+					"A) The Lord of the Rings trilogy",
+					"B) Gone Girl",
+					"C) The Fault in Our Stars",
+					"D) The Great Gatsby",
+				],
+			},
+			{
+				id: 3,
+				question: "Which type of book dedication attracts you?",
+				options: [
+					"A) To the dreamers",
+					"B) To the truth-seekers",
+					"C) To the lovers",
+					"D) To the thinkers",
+				],
+			},
+			{
+				id: 4,
+				question: "What kind of movies/TV do you like to watch the most?",
+				options: [
+					"A) Something with magical creatures, or a different world that I can get lost in",
+					"B) Something that I'll need to think about or solve. May or may not have plot twists",
+					"C) Something relatable, funny, or dramatic",
+					"D) Something about a real person or an event that may or may not be a documentary",
+				],
+			},
+			{
+				id: 5,
+				question: "Which of the following sounds like a great getaway?",
+				options: [
+					"A) In a space station determining the course of your next hyperdrive destination",
+					"B) Somewhere that's relaxing and where there's no drama",
+					"C) A lavish hotel in a city shrouded in mystery",
+					"D) A secluded cabin in the woods",
+				],
+			},
 		],
 	},
 	{
@@ -16,46 +61,162 @@ const QuestionsData = [
 				id: 6,
 				question: "What kind of ending do you prfer?",
 				options: [
-					"A) A triumphant victory like in The Lord of the Rings",
-					"B) An unexpected but logical conclusion like in Shutter Island",
-					"C) A happy ever after like in To All the Boys I've Loved Before",
-					"D) An open-ended finish like in Slaughterhouse-Five",
+					"A) A triumphant victory like in 'The Lord of the Rings'",
+					"B) An unexpected but logical conclusion like in 'Shutter Island'",
+					"C) A happy ever after like in To 'All the Boys I've Loved Before'",
+					"D) An open-ended finish like in 'Slaughterhouse-Five'",
 				],
 			},
-			{ id: 7, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 8, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 9, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 10, question: "Question 2?", options: ["A", "B", "C", "D"] },
+			{
+				id: 7,
+				question: "What do you NOT like to read about?",
+				options: [
+					"A) I cannot read anything about boring, regular people. I want suspense!",
+					"B) I don't like to read books with painfully obvious plots",
+					"C) I really dislike happy endings. Where's the fun in that?",
+					"D) I really don't like to read about magical worlds. It's just not believable to me",
+				],
+			},
+			{
+				id: 8,
+				question: "Which type of narrative conflict do you prefer?",
+				options: [
+					"A) Character vs. supernatural",
+					"B) Character vs. man",
+					"C) Character vs. love",
+					"D) Character vs. self",
+				],
+			},
+			{
+				id: 9,
+				question: "What type of narrative style do you prefer?",
+				options: [
+					"A) Third-person with multiple perspectives",
+					"B) First-person with unreliable narrators",
+					"C) First-person with relatable voice",
+					"D) Stream of consciousness",
+				],
+			},
+			{
+				id: 10,
+				question: "When’s the best time to read a book?",
+				options: [
+					"A)Round the campfire",
+					"B)In class",
+					"C)On the bus",
+					"D)All the time",
+				],
+			},
 		],
 	},
 	{
 		clusterId: 3,
 		questions: [
-			{ id: 11, question: "Question 1?", options: ["A", "B", "C", "D"] },
-			{ id: 12, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 13, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 14, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 15, question: "Question 2?", options: ["A", "B", "C", "D"] },
+			{
+				id: 11,
+				question: "Which type of character do you relate to the most?",
+				options: [
+					"A) The chosen one like 'Harry'",
+					"B) The detective like 'Sherlock Holmes'",
+					"C) The romantic like Lara Jean from To 'All the Boys I’ve Loved Before'",
+					"D) The introspective thinker like Holden from 'The Catcher in the Rye'",
+				],
+			},
+			{
+				id: 12,
+				question: "Which fictional general would you appoint to lead the army?",
+				options: [
+					"A) Erwin Smith",
+					"B) Jon Snow",
+					"C) Aragorn",
+					"D) Wilhuff Tarkin",
+				],
+			},
+			{
+				id: 13,
+				question: "What kind of book do you prefer for a long read?",
+				options: [
+					"A) Epic fantasy series like 'The Wheel of Time'",
+					"B) Complex mysteries like 'The Cuckoo’s Calling'",
+					"C) Romantic sagas like 'Outlander'",
+					"D) Comprehensive philosophical novels like 'Infinite Jest'",
+				],
+			},
+			{
+				id: 14,
+				question: "Which kind of plot devices do you enjoy?",
+				options: [
+					"A) Magical artifacts or prophecies",
+					"B) Red herrings and plot twists",
+					"C) Meet-cutes and love triangles",
+					"D) Flashbacks and inner monologues",
+				],
+			},
+			{
+				id: 15,
+				question: "Which type of book setting appeals to you?",
+				options: [
+					"A) A magical school like in Hogwarts",
+					"B) A small, creepy town like in Castle Rock",
+					"C) A charming city like in Paris from Anna and the French Kiss",
+					"D) An abstract or dystopian world like in The Handmaid's Tale",
+				],
+			},
 		],
 	},
 	{
 		clusterId: 4,
 		questions: [
-			{ id: 16, question: "Question 1?", options: ["A", "B", "C", "D"] },
-			{ id: 17, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 18, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 19, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 20, question: "Question 2?", options: ["A", "B", "C", "D"] },
-		],
-	},
-	{
-		clusterId: 5,
-		questions: [
-			{ id: 21, question: "Question 1?", options: ["A", "B", "C", "D"] },
-			{ id: 22, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 23, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 24, question: "Question 2?", options: ["A", "B", "C", "D"] },
-			{ id: 25, question: "Question 2?", options: ["A", "B", "C", "D"] },
+			{
+				id: 16,
+				question: "Which type of book title intrigues you?",
+				options: [
+					"A) Titles with magical or mythical elements like 'The Priory of the Orange Tree'",
+					"B) Titles suggesting suspense and danger like The 'Girl on the Train'",
+					"C) Titles hinting at love or humor like To All the Boys 'The Both Die at the End'",
+					"D) Titles that are thought-provoking like A 'Brief History of Time'",
+				],
+			},
+			{
+				id: 17,
+				question: "When reading, you care most about...",
+				options: [
+					"A) The world",
+					"B) The twists",
+					"C) The characters",
+					"D) The dialogue",
+				],
+			},
+			{
+				id: 18,
+				question: "Which time period do you prefer your books to be set in?",
+				options: [
+					"A) A distant past or future like in 'The Chronicles of Narnia'",
+					"B) Modern day with dark undertones like in 'The Silent Patient'",
+					"C) Contemporary setting like in 'Bridget Jones's Diary'",
+					"D) Timeless periods like in 'Siddhartha'",
+				],
+			},
+			{
+				id: 19,
+				question: "Which type of character relationships do you enjoy?",
+				options: [
+					"A) Allies and foes",
+					"B) Suspects and detectives",
+					"C) Lovers and friends",
+					"C) Lovers and friends",
+				],
+			},
+			{
+				id: 20,
+				question: "And lastly, what's your favorite part about reading a book?",
+				options: [
+					"A) I love when books make my heart beat faster",
+					"B) I love when a book keeps me guessing",
+					"C) I love when a book makes me laugh and cry",
+					"D) I love when I can use my imagination",
+				],
+			},
 		],
 	},
 ];
