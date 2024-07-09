644d74f6b1a1254a149d3f1517a6acde7e3cc3c4
arunnats <arun_b220019cs@nitc.ac.in>
2024-07-09 13:59:59 +0530

    responsiveness

--- App/frontend/BookMate/src/components/ReccInfo/ReccInfo.jsx
+++ App/frontend/BookMate/src/components/ReccInfo/ReccInfo.jsx
@@ -32,21 +32,21 @@ const ReccInfo = () => {
 	return (
 		<div className={`relative bg-primary  flex items-center justify-center `}>
 			<div className="text-center text-neutral-content w-full overflow-hidden">
-				<div className="flex flex-row min-w-[100vw] m-auto p-3">
-					<div className=" w-[4%] flex flex-col-reverse"></div>
-					<div className=" w-[36%] flex items-center justify-center">
+				<div className="flex flex-col md:flex-row min-w-[100vw] m-auto p-1 md:p-3">
+					<div className=" max-w-[0%] md:max-w-[4%] flex flex-col-reverse"></div>
+					<div className=" w-[80%] md:w-[36%] flex items-center justify-center mx-auto">
 						<img
 							ref={imgRef}
 							src={reccLogo}
 							alt="Hero"
-							className="max-h-[50vh] object-contain align-middle justify-center p-5"
+							className="max-h-[35vh] md:max-h-[50vh] object-contain align-middle justify-center p-5"
 						/>
 					</div>
-					<div className=" w-[52%] flex flex-col items-center justify-center">
-						<h1 className="text-secondary font-poppins font-bold text-4xl">
+					<div className=" w-full md:w-[52%] flex flex-col items-center justify-center">
+						<h1 className="text-secondary font-poppins font-bold text-3xl md:text-4xl 2xl:text-5xl ">
 							Book Recommendations
 						</h1>
-						<p className="text-lg text-accent font-montserrat font-medium text-justify p-1">
+						<p className="text-xl 2xl:text-2xl text-accent font-montserrat font-medium text-justify p-5 md:p-1">
 							Find your next favorite book with our meticulous AI model, trained
 							on over 1,149,000 ratings from over 278,000 users across the
 							world! Simply choose a book you like and get curated results in an
@@ -56,7 +56,7 @@ const ReccInfo = () => {
 							to get random book titles.
 						</p>
 					</div>
-					<div className=" w-[8%] flex flex-col-reverse items-center"></div>
+					<div className=" w-[0%] md:w-[8%] flex flex-col-reverse items-center"></div>
 				</div>
 			</div>
 		</div>
